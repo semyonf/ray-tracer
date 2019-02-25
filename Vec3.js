f2c07fa2bf9951ff63da4110b7b8b6d45d3e1a5e
Semyon Fomin <taxnuke@gmail.com>
2019-02-26 02:29:41 +0300

    Add Vec3 class

---
+++ Vec3.js
@@ -0,0 +1,38 @@
+export class Vec3 {
+  /**
+   * A three-dimensional vector
+   * @param {number} x 
+   * @param {number} y 
+   * @param {number} z 
+   */
+  constructor(x = 0, y = 0, z = 0) {
+    this.x = x
+    this.y = y
+    this.z = z
+  }
+  add(other) {
+    return new Vec3(this.x + other.x, this.y + other.y, this.z + other.z)
+  }
+  sub(other) {
+    return new Vec3(this.x - other.x, this.y - other.y, this.z - other.z)
+  }
+  calcNorm() {
+    return Math.sqrt(this.dotProduct(this))
+  }
+  scale(factor) {
+    return new Vec3(this.x * factor, this.y * factor, this.z * factor)
+  }
+  normalize() {
+    return this.scale(1 / this.calcNorm())
+  }
+  xProduct(other) {
+    return new Vec3(
+      this.y * other.z - this.z * other.y,
+      this.z * other.x - this.x * other.z,
+      this.x * other.y - this.y * other.x
+    )
+  }
+  dotProduct(other) {
+    return this.x * other.x + this.y * other.y + this.z * other.z
+  }
+}
